ad94e35e556bfd126f23027fee0c0cfa4031f099
LocNguyen <binbinvn@gmail.com>
2018-01-02 09:59:49 +0700

    added bootstrap gem

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,6 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		# render plain: params[:article].inspect
 		@article = Article.new(article_params)
 		if @article.save
 			flash[:notice] = "Article was successfully created"
